ffdbfca433af0da129b366867fae1c57d7e9ff47
Davi <daviboyistop@yahoo.com>
2024-08-24 10:31:55 -0300

    Create Keynolol.lua

---
+++ .github/Scriptcoolbyfadavid/Keynolol.lua
@@ -0,0 +1,4 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "fam hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
